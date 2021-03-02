c756d92b2d70d5e19c31f5808427d8ab93939b63
Donritchie-E <66331432+Donritchie-E@users.noreply.github.com>
2021-03-01 22:50:03 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-pipelinefhjksahf"
     key    = "state/jjtech.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
     profile = "default"
   }
 }
